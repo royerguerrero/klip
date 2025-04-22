bdb328990823b1a884114e5e6106f3730c76da61
Royer Guerrero <royjuni3431@gmail.com>
2025-04-22 15:21:00 -0500

    debug: auth problems

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -31,7 +31,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         try {
           const query = new AuthenticateUserQuery(
             credentials.email as string,
-            credentials.password as string,
+            credentials.password as string
           );
           const user =
             await bootstrap.queryBus.ask<AuthenticateUserQueryResponse>(query);
@@ -40,6 +40,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           if (error instanceof InvalidCredentials) {
             return null;
           }
+
+          throw error;
         }
 
         return null;
